4644d341f3c273b2a8b09c85b365b6fb29dae9e2
n03fs <nahiafs@iescastelar.com>
2022-11-28 23:15:27 +0100

    Segundo commit. Movimiento personaje

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -10,12 +10,14 @@ public class MovimientoPersonaje : MonoBehaviour
     private float MovimientoCamaraX;
     private float MovimientoCamaraY;
     private float VelocidadMovimiento = 5.0f;
+    private float VelocidadCorrer = 10.0f;
     private float VelocidadGiro = 50.0f;
+    private Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -27,12 +29,28 @@ public class MovimientoPersonaje : MonoBehaviour
         MovimientoCamaraX = Input.GetAxis("Mouse X") * VelocidadGiro * Time.deltaTime;
         MovimientoCamaraY = Input.GetAxis("Mouse Y") * VelocidadGiro * Time.deltaTime;
         
-        Vector3 movimiento = new Vector3(MovimientoHorizontal, MovimientoSalto, MovimientoVertical);
-        transform.Translate(movimiento * VelocidadMovimiento * Time.deltaTime);
+        Vector3 movimiento = new Vector3(MovimientoHorizontal, 0f, MovimientoVertical);
+        
+        if (MovimientoHorizontal <= 0 && MovimientoVertical <= 0)
+        {
+            animator.SetFloat("Blend", 0f);
+        } 
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(movimiento * VelocidadCorrer * Time.deltaTime);
+            animator.SetFloat("Blend", 1f);
+        }
+        else
+        {
+            transform.Translate(movimiento * VelocidadMovimiento * Time.deltaTime);
+            animator.SetFloat("Blend", 0.5f);
+        }
+        
+        transform.Rotate(new Vector3(0, 1, 0), MovimientoHorizontal * VelocidadGiro * Time.deltaTime);
 
-        //Vector3 rotacion = new Vector3(MovimientoCamaraX, 0f, MovimientoCamaraY);
-        //transform.Rotate(rotacion * VelocidadGiro * Time.deltaTime);
-        
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            transform.Translate(transform.up * MovimientoSalto * Time.deltaTime);
+        }
     }
 }
